3643598540852e175460d53a53f9a17c76b8c0b9
rachel8365 <rv8365@gmail.com>
2023-06-23 12:38:06 +0300

    commit

--- products.js
+++ products.js
@@ -25,36 +25,63 @@ let products = [
             "Offering a formidable core experience for serious gaming and multitasking on Windows 10 Pro",
         image: "../images/asus.jpg",
     },
+    {
+        id: 2539,
+        name: "A laser printer that includes a Wi-Fi connection ",
+        price: 450,
+        category: "Printers",
+        description:
+            "Xerox Phaser 3020 laser printer that includes a Wi-Fi connection and printing from AirPrint",
+        image: "images/printer.jpg",
+    },
+    {
+        id: 2540,
+        name: "A desktop computer with an Intel i5-11400 processor, 16GB memory, 480GB SSD drive",
+        price: 2499,
+        category: "computers",
+        description: "Intel Core i5-11400 processor, Asus H510 motherboard, 480GB SSD drive, 16GB DDR4 memory, Windows 11 Home operating system",
+        image: "../images/windows.jpg"
+    },
+    {
+        id: 2541,
+        name: "OnePlus Nord N20 SE 128GB smartphone",
+        price: 699,
+        category: "smartfhons",
+        description: "OnePlus Nord N20 SE 128GB smartphone in black color - two years warranty by the official importer",
+        image: "../images/smart.jpg"
+    }
 ];
 
-// לכתוב פונקציה שתפקידה להציג את המוצרים בקארדים של בוטסטרפ
-// שלושה קארדים בשורה
-// תמונה של המוצר, שם המוצר, קטגוריה וכפתור לפרטים מלאים
 
 function showCards() {
     for (let i = 0; i < products.length; i++) {
-        document.getElementById("products").innerHTML += `
-        <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-  <img src=${products[i].image} class="card-img-top" alt=${products[i].name}>
-  <div class="card-body">
-    <h5 class="card-title">${products[i].name}</h5>
-    <p class="card-text">${products[i].category}</p>
-    <a data-bs-toggle="modal" data-bs-target="#infoModal" class="btn btn-primary w-100" onclick="setModal(${i})">see more</a>
-  </div>
-</div>
-        </div>
-        `;
+        document.getElementById("products").innerHTML +=
+            `<div class="col-lg-4 col-md-4 col-sm-12 mt-5">
+            <div class="card w-20 h-100">
+            <img src=${products[i].image} class="card-img-top" alt=${products[i].name}>
+            <div class="card-body">
+            <h5 class="card-title">${products[i].name}</h5>
+            <p class="card-text">${products[i].category}</p>
+            </div>
+                        
+        <button class="card-footer btn btn-primary " data-bs-toggle="modal" data-bs-target="#infoModal" onclick=setModal(${i})>See More</button>       
+            
+            </div>
+        </div>`
     }
 }
-
 showCards();
-// לכתוב פונקציה שמזינה את פרטי המוצר למודאל על סמך אינדקס
 
-// לכתוב פונקציה שמזינה את פרטי המוצר למודאל על סמך אינדקס
+
 function setModal(index) {
     document.getElementById("title").innerText = products[index].name;
     document.getElementById("mbody").innerHTML = `
   <p><b>Serial Number:</b> ${products[index].id}</p><p><b>Category:</b> ${products[index].category}</p><p><b>Description:</b> ${products[index].description}</p>
-  <h4 class="text-end">Price: ${products[index].price} </h4>`;
+  <a onClick=favorite()><i class="fa-regular fa-heart"></i></a/>
+  <h4 class="text-end">Price: &#8362; ${products[index].price}  </h4> `;
 }
+
+function favorite() {
+    document.getElementById("title").innerText = `Added to favorites`
+    document.getElementById("mbody").innerHTML = `<i class="fa-solid fa-heart"></i>`
+} 
